7dd85b6e3139ff860207d000746db12d3cf41de4
aifyer <aifyer@aifyers-MacBook-Air.local>
2022-10-04 00:41:43 -0700

    I can't find R presentation in my R studio file. so I used R markdown knitr to presentation. but the format is not so beautiful. to be safe, I commit original R markdown file too.

---
+++ explanation_for_slide_deck.R
@@ -0,0 +1,5 @@
+print("I can't find R Presentation in my R studio file")
+
+print("Tried Quarto Presentation. just render similar slides.")
+
+print("That's why I push .Rmd file too. You can easily find it in the repository")
